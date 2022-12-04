2424e9c5ab0c930bbfdb823d257227983bf850a5
Jan Piñol Castuera <jan.pinol@students.salle.url.edu>
2022-12-04 01:06:10 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import Year2022.DayN;
 import Year2022.Day1;
 import Year2022.Day2;
+import Year2022.Day3;
 
 public class Main {
 
@@ -8,7 +9,7 @@ public class Main {
         //DayN.main();
         //Day1.main();
         //Day2.main();
-        //Day3.main();
+        Day3.main();
     }
 
 }
---
+++ src/Year2022/Day3.java
@@ -0,0 +1,106 @@
+package Year2022;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+
+public class Day3 {
+    private ArrayList<String> inputString;
+
+    public Day3() {
+        this.inputString = new ArrayList<>();
+    }
+
+    public void mainProcessDay3Gold() {
+        int total = 0;
+        for (int i = 0; i < inputString.size(); i++) {
+            String[] parts1 = inputString.get(i).split("");
+            String[] parts2 = inputString.get(i+1).split("");
+            String[] parts3 = inputString.get(i+2).split("");
+
+            boolean added = false;
+            for (int j = 0; j < parts1.length; j++) {
+                for (int n = 0; n < parts2.length ; n++) {
+                    if (parts1[j].charAt(0) == parts2[n].charAt(0)) {
+                        for (int k = 0; k < parts3.length ; k++) {
+                            if (parts2[n].charAt(0) == parts3[k].charAt(0)) {
+                                if (Character.isUpperCase(parts1[j].charAt(0))){
+                                    System.out.print(parts1[j].charAt(0) - 38);
+                                    System.out.println(parts1[j]);
+
+                                    total += parts1[j].charAt(0) - 38;
+                                }else {
+                                    System.out.print(parts1[j].charAt(0) - 96);
+                                    System.out.println(parts1[j]);
+
+                                    total += parts1[j].charAt(0) - 96;
+                                }
+                                added = true;
+                                break;
+                            }
+                        }
+                    }
+                    if(added) {
+                        break;
+                    }
+                }
+            }
+            i += 2;
+        }
+        System.out.println(total);
+    }
+
+    public void mainProcessDay3Silver() {
+        int total = 0;
+        for (int i = 0; i < inputString.size(); i++) {
+            String[] parts = inputString.get(i).split("");
+            boolean added = false;
+            for (int j = 0; j < parts.length / 2; j++) {
+                for (int n = 0; n < parts.length ; n++) {
+                    if (n >= parts.length / 2) {
+                        if (parts[j].charAt(0) == parts[n].charAt(0)) {
+                            if (Character.isUpperCase(parts[j].charAt(0))){
+                                total += parts[j].charAt(0) - 38;
+                            }else {
+                                total += parts[j].charAt(0) - 96;
+                            }
+                            added = true;
+                            break;
+                        }
+                    }
+                }
+                if(added) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main() {
+        // Initiate constructor
+        Day3 main = new Day3();
+
+        // Read input.txt file and fill input array string
+        main.readFile();
+
+        // Execute the algorithm
+        //main.mainProcessDay3Silver();
+        main.mainProcessDay3Gold();
+    }
+
+    public void readFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                inputString.add(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
